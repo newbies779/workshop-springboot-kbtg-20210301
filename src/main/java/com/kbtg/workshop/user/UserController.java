9825d6fff9c912d90ff687d38a4957ba30609785
Anupong Chuen-Im <anupong.c@kbtg.tech>
2021-03-01 17:08:40 +0700

    add Swagger

---
+++ src/main/java/com/kbtg/workshop/user/UserController.java
@@ -0,0 +1,14 @@
+package com.kbtg.workshop.user;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/user/{id}")
+    public UserResponse getUserById(@PathVariable int id) {
+        return new UserResponse(id, "test", 30);
+    }
+}
